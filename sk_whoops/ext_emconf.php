71d2662a9babe14dd608cbb6db129e9b834abf5f
Steffen Kamper <info@sk-typo3.de>
2016-11-03 10:41:59 +0100

    Add files via upload
    Init

---
+++ sk_whoops/ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "sk_whoops".
+ *
+ * Auto generated 17-06-2016 11:43
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'SK Whoops',
+  'description' => 'Integrate whoops into TYPO3',
+  'category' => 'plugin',
+  'version' => '1.0.0',
+  'state' => 'stable',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'clearcacheonload' => 0,
+  'author' => 'Steffen Kamper',
+  'author_email' => 'info@sk-typo3.de',
+  'author_company' => '',
+  'constraints' =>
+    array (
+      'depends' =>
+        array (
+          'typo3' => '6.2.0-7.99.99',
+        ),
+      'conflicts' =>
+        array (
+        ),
+      'suggests' =>
+        array (
+        ),
+    ),
+);
+
